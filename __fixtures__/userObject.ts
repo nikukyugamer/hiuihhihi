337d0d7ad63ba75ac4f651d80cdb9db4a2dd477d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-03-25 20:56:07 +0900

    feat: 🎸 リストのタイムラインを取得する最低限のひな形のコードを追加した (#41)

---
+++ __fixtures__/userObject.ts
@@ -0,0 +1,125 @@
+module.exports.userObject = {
+  id: 150552419,
+  id_str: '150552419',
+  name: 'KONAMI コナミ公式',
+  screen_name: 'KONAMI573ch',
+  location: '東京都中央区銀座',
+  profile_location: null,
+  description:
+    'コナミデジタルエンタテインメントの公式アカウントです。\n' +
+    'KONAMIの商品・サービス情報を宣伝チームのコナちゃんがお届けします！\n' +
+    '※商品・サービスのお問い合わせ➡️https://t.co/DFA9JyQSEQ\n' +
+    '※当社SNS利用方針➡️https://t.co/XbbdZLbVzd',
+  url: 'https://t.co/oHzFCci9DI',
+  entities: { url: { urls: [Array] }, description: { urls: [Array] } },
+  protected: false,
+  followers_count: 83343,
+  friends_count: 16199,
+  listed_count: 1586,
+  created_at: 'Tue Jun 01 06:25:10 +0000 2010',
+  favourites_count: 7272,
+  utc_offset: null,
+  time_zone: null,
+  geo_enabled: false,
+  verified: true,
+  statuses_count: 67223,
+  lang: null,
+  status: {
+    created_at: 'Fri Mar 25 09:11:47 +0000 2022',
+    id: 1507284097965600800,
+    id_str: '1507284097965600770',
+    full_text:
+      'RT @prospiA_PR: #プロスピA プロ野球選手対決動画 東京ヤクルト編\n' +
+      '\n' +
+      '＃清水昇 選手の切り札は ＃青木宣親 選手？！\n' +
+      '笑いの絶えない対決動画が公開中🎬\n' +
+      '打たないでとゲーム内の自分に願うチャーミングな青木選手にも注目だよ😊 もちろんエキサイティングプレゼントも🎁…',
+    truncated: false,
+    display_text_range: [0, 139],
+    entities: {
+      hashtags: [Array],
+      symbols: [],
+      user_mentions: [Array],
+      urls: [],
+    },
+    source:
+      '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+    in_reply_to_status_id: null,
+    in_reply_to_status_id_str: null,
+    in_reply_to_user_id: null,
+    in_reply_to_user_id_str: null,
+    in_reply_to_screen_name: null,
+    geo: null,
+    coordinates: null,
+    place: null,
+    contributors: null,
+    retweeted_status: {
+      created_at: 'Fri Mar 25 09:10:00 +0000 2022',
+      id: 1507283647677706200,
+      id_str: '1507283647677706242',
+      full_text:
+        '#プロスピA プロ野球選手対決動画 東京ヤクルト編\n' +
+        '\n' +
+        '＃清水昇 選手の切り札は ＃青木宣親 選手？！\n' +
+        '笑いの絶えない対決動画が公開中🎬\n' +
+        '打たないでとゲーム内の自分に願うチャーミングな青木選手にも注目だよ😊 もちろんエキサイティングプレゼントも🎁\n' +
+        '【動画はこちら👇】\n' +
+        'https://t.co/Ymg1pehJg0',
+      truncated: false,
+      display_text_range: [Array],
+      entities: [Object],
+      source:
+        '<a href="https://mobile.twitter.com" rel="nofollow">Twitter Web App</a>',
+      in_reply_to_status_id: null,
+      in_reply_to_status_id_str: null,
+      in_reply_to_user_id: null,
+      in_reply_to_user_id_str: null,
+      in_reply_to_screen_name: null,
+      geo: null,
+      coordinates: null,
+      place: null,
+      contributors: null,
+      is_quote_status: false,
+      retweet_count: 52,
+      favorite_count: 518,
+      favorited: false,
+      retweeted: false,
+      possibly_sensitive: false,
+      lang: 'ja',
+    },
+    is_quote_status: false,
+    retweet_count: 52,
+    favorite_count: 0,
+    favorited: false,
+    retweeted: false,
+    lang: 'ja',
+  },
+  contributors_enabled: false,
+  is_translator: false,
+  is_translation_enabled: false,
+  profile_background_color: '000000',
+  profile_background_image_url:
+    'http://abs.twimg.com/images/themes/theme4/bg.gif',
+  profile_background_image_url_https:
+    'https://abs.twimg.com/images/themes/theme4/bg.gif',
+  profile_background_tile: false,
+  profile_image_url:
+    'http://pbs.twimg.com/profile_images/1060355968360341505/x6DD3Aua_normal.jpg',
+  profile_image_url_https:
+    'https://pbs.twimg.com/profile_images/1060355968360341505/x6DD3Aua_normal.jpg',
+  profile_banner_url:
+    'https://pbs.twimg.com/profile_banners/150552419/1607590319',
+  profile_link_color: 'B60014',
+  profile_sidebar_border_color: '000000',
+  profile_sidebar_fill_color: '000000',
+  profile_text_color: '000000',
+  profile_use_background_image: false,
+  has_extended_profile: true,
+  default_profile: false,
+  default_profile_image: false,
+  following: false,
+  follow_request_sent: false,
+  notifications: false,
+  translator_type: 'none',
+  withheld_in_countries: [],
+}
