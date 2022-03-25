cc5d9f454c2f39097495fae687d3c8ea262e31ae
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-03-25 17:44:45 +0900

    feat: 🎸 apps 部分と lib 部分 を分けた

--- lib/twitterClient.ts
+++ lib/twitterClient.ts
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { TwitterApi } = require('twitter-api-v2')
 
-const sampleTwitterApp = async function () {
+exports.sampleTwitterApp = async function () {
   const userClient = new TwitterApi({
     appKey: process.env.CONSUMER_APP_KEY,
     appSecret: process.env.CONSUMER_APP_SECRET,
@@ -15,8 +15,6 @@ const sampleTwitterApp = async function () {
   const me = await userClient.v1.user({ screen_name: 'KONAMI573ch' })
   console.log(me)
 }
-
-sampleTwitterApp()
 
 // {
 //   id: 150552419,
