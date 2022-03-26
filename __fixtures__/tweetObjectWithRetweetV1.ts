222df2812a52752e6473f2b10bfb2a45a223863c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-03-26 13:59:00 +0900

    test: 💍 リツイートに関する Spec を追加した (#46)

---
+++ __fixtures__/tweetObjectWithRetweetV1.ts
@@ -0,0 +1,189 @@
+module.exports.tweetObjectWithRetweetV1 = {
+  created_at: 'Sat Mar 26 02:35:58 +0000 2022',
+  id: 1507546875557126100,
+  id_str: '1507546875557126145',
+  full_text:
+    'RT @pina_xx: 🚨タイテ解禁🚨\n' +
+    '#あにめないと ▶︎4.1(金)23:00〜\n' +
+    '場所▶︎▶︎秋葉原 #MOGRA\n' +
+    '\n' +
+    '✅ルールをおさらい✅\n' +
+    '❗️メジャーなアニソンしか流れない\n' +
+    '❗️フロアの皆が知らないと、ブース前のランプが点灯\n' +
+    '❗️5つ点灯でDJにテキーラです🥃\n' +
+    '\n' +
+    ' タイ…',
+  truncated: false,
+  display_text_range: [0, 140],
+  entities: {
+    hashtags: [[Object], [Object]],
+    symbols: [],
+    user_mentions: [[Object]],
+    urls: [],
+  },
+  source:
+    '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+  in_reply_to_status_id: null,
+  in_reply_to_status_id_str: null,
+  in_reply_to_user_id: null,
+  in_reply_to_user_id_str: null,
+  in_reply_to_screen_name: null,
+  user: {
+    id: 80520369,
+    id_str: '80520369',
+    name: 'MOGRA',
+    screen_name: 'MOGRAstaff',
+    location: '東京都台東区秋葉原3-11,松菱ビル1F&B1',
+    description:
+      '「株式会社モグラ」及び「秋葉原MOGRA」の公式Twitterです。 イベントや配信情報、当日の様子などを不定期にポストします。お問い合わせは公式サイトをご覧ください。 #mogra #MU2020 #あきねっと #akinet #TLTC #MU2022 https://t.co/HXeTJHB2c9',
+    url: 'https://t.co/rqSwkv4b1d',
+    entities: { url: [Object], description: [Object] },
+    protected: false,
+    followers_count: 21465,
+    friends_count: 498,
+    listed_count: 701,
+    created_at: 'Wed Oct 07 07:35:46 +0000 2009',
+    favourites_count: 841,
+    utc_offset: null,
+    time_zone: null,
+    geo_enabled: false,
+    verified: false,
+    statuses_count: 25079,
+    lang: null,
+    contributors_enabled: false,
+    is_translator: false,
+    is_translation_enabled: false,
+    profile_background_color: 'C0DEED',
+    profile_background_image_url:
+      'http://abs.twimg.com/images/themes/theme1/bg.png',
+    profile_background_image_url_https:
+      'https://abs.twimg.com/images/themes/theme1/bg.png',
+    profile_background_tile: true,
+    profile_image_url:
+      'http://pbs.twimg.com/profile_images/1114492012932108288/uwZ1JHBQ_normal.png',
+    profile_image_url_https:
+      'https://pbs.twimg.com/profile_images/1114492012932108288/uwZ1JHBQ_normal.png',
+    profile_banner_url:
+      'https://pbs.twimg.com/profile_banners/80520369/1554550769',
+    profile_link_color: 'E40077',
+    profile_sidebar_border_color: 'C0DEED',
+    profile_sidebar_fill_color: 'DDEEF6',
+    profile_text_color: '333333',
+    profile_use_background_image: true,
+    has_extended_profile: true,
+    default_profile: false,
+    default_profile_image: false,
+    following: false,
+    follow_request_sent: false,
+    notifications: false,
+    translator_type: 'none',
+    withheld_in_countries: [],
+  },
+  geo: null,
+  coordinates: null,
+  place: null,
+  contributors: null,
+  retweeted_status: {
+    created_at: 'Fri Mar 25 12:00:33 +0000 2022',
+    id: 1507326570520019000,
+    id_str: '1507326570520018948',
+    full_text:
+      '🚨タイテ解禁🚨\n' +
+      '#あにめないと ▶︎4.1(金)23:00〜\n' +
+      '場所▶︎▶︎秋葉原 #MOGRA\n' +
+      '\n' +
+      '✅ルールをおさらい✅\n' +
+      '❗️メジャーなアニソンしか流れない\n' +
+      '❗️フロアの皆が知らないと、ブース前のランプが点灯\n' +
+      '❗️5つ点灯でDJにテキーラです🥃\n' +
+      '\n' +
+      ' タイテも解禁です！\n' +
+      'まん防開けあにない、思う存分楽しみましょ✊ https://t.co/4RcONYkbLX',
+    truncated: false,
+    display_text_range: [0, 155],
+    entities: {
+      hashtags: [Array],
+      symbols: [],
+      user_mentions: [],
+      urls: [],
+      media: [Array],
+    },
+    extended_entities: { media: [Array] },
+    source:
+      '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+    in_reply_to_status_id: null,
+    in_reply_to_status_id_str: null,
+    in_reply_to_user_id: null,
+    in_reply_to_user_id_str: null,
+    in_reply_to_screen_name: null,
+    user: {
+      id: 371608383,
+      id_str: '371608383',
+      name: 'まこ',
+      screen_name: 'pina_xx',
+      location: '',
+      description:
+        '真っ赤なランプとテキーラが飛び交う #あにめないと🚨 主催 / イラストとデザイン / omaju08✴︎https://t.co/nIAiGq9x5n /',
+      url: null,
+      entities: [Object],
+      protected: false,
+      followers_count: 863,
+      friends_count: 642,
+      listed_count: 9,
+      created_at: 'Sun Sep 11 05:21:22 +0000 2011',
+      favourites_count: 17735,
+      utc_offset: null,
+      time_zone: null,
+      geo_enabled: true,
+      verified: false,
+      statuses_count: 14657,
+      lang: null,
+      contributors_enabled: false,
+      is_translator: false,
+      is_translation_enabled: false,
+      profile_background_color: 'C0DEED',
+      profile_background_image_url:
+        'http://abs.twimg.com/images/themes/theme1/bg.png',
+      profile_background_image_url_https:
+        'https://abs.twimg.com/images/themes/theme1/bg.png',
+      profile_background_tile: false,
+      profile_image_url:
+        'http://pbs.twimg.com/profile_images/1180753672059273216/rC9vI0u5_normal.jpg',
+      profile_image_url_https:
+        'https://pbs.twimg.com/profile_images/1180753672059273216/rC9vI0u5_normal.jpg',
+      profile_banner_url:
+        'https://pbs.twimg.com/profile_banners/371608383/1416234368',
+      profile_link_color: '1DA1F2',
+      profile_sidebar_border_color: 'C0DEED',
+      profile_sidebar_fill_color: 'DDEEF6',
+      profile_text_color: '333333',
+      profile_use_background_image: true,
+      has_extended_profile: true,
+      default_profile: true,
+      default_profile_image: false,
+      following: false,
+      follow_request_sent: false,
+      notifications: false,
+      translator_type: 'none',
+      withheld_in_countries: [],
+    },
+    geo: null,
+    coordinates: null,
+    place: null,
+    contributors: null,
+    is_quote_status: false,
+    retweet_count: 11,
+    favorite_count: 2,
+    favorited: false,
+    retweeted: false,
+    possibly_sensitive: false,
+    possibly_sensitive_appealable: false,
+    lang: 'ja',
+  },
+  is_quote_status: false,
+  retweet_count: 11,
+  favorite_count: 0,
+  favorited: false,
+  retweeted: false,
+  lang: 'ja',
+}
